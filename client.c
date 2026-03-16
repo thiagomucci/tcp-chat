614d05fe96ef991b7da2c49405306eb8e96a1726
thiagomucci <thmucc@protonmail.com>
2026-03-16 20:48:18 -0300

    added main client

---
+++ client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAXBUF 4096
+
+int
+main(int argc, char *argv[])
+{}
